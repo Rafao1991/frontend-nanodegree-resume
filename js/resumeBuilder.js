f92cc2cc68f5ab13fa1c8f669f8b88dd87847b69
Rafael Sousa <1c302489@gmail.com>
2016-08-22 22:56:54 -0300

    add some bio info to the resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,45 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//$("#main").append(["Rafael Sousa"]);
+
+// var awesomeThouhts = "I am Rafael Sousa and I am AWESOME!"
+// console.log(awesomeThouhts);
+
+// var funThouhts = awesomeThouhts.replace("AWESOME", "FUN");
+// $("#main").append(funThouhts);
+
+var bio = {
+	name : "Rafael Sousa ",
+	role : "Software Developer ",
+	contact : {
+		mobile : "+55 11 99159-9573",
+		email : "rcsva_sousa@hotmail.com",
+		github : "Rafao1991",
+		location : "São Paulo"
+	},
+	pictureURL : "images/me.jpg",
+	welcomeMessae : "Welcome to my resume I expect that you like my bio and contact me to exchange knowledge.",
+	skills : [
+		" Mobile development",
+		" Javascript",
+		" Agile"
+	]
+};
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contact.github);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contact.location);
+var formattedPic = HTMLbioPic.replace("%data%", bio.pictureURL);
+var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessae);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+
+$("#header").prepend(formattedSkills);
+$("#header").prepend(formattedWelcomeMessage);
+$("#header").prepend(formattedPic);
+$("#header").prepend(formattedLocation);
+$("#header").prepend(formattedGithub);
+$("#header").prepend(formattedEmail);
+$("#header").prepend(formattedMobile);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
